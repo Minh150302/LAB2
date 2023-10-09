02ec20d4a0edac5518f90be17a03370da7a16455
LAPTOP-MINH\Huy Minh <hoanghuyminh1503@gmail.com>
2023-10-09 12:06:35 +0700

    INIT EX9

---
+++ LAB2/Core/Inc/software_timer.h
@@ -0,0 +1,17 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 9, 2023
+ *      Author: Huy Minh
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+
+void setTimer1(int duration);
+void timerRun();S
+
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
